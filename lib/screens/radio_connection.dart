532f8b8a5daa15f377b9cd52d1b6a8d6e4daa7ec
paulo <paulo@paulos-Mac-mini.local>
2024-03-16 02:29:25 +0800

    use LayoutBuilder on radio_connection.dart

--- lib/screens/radio_connection.dart
+++ lib/screens/radio_connection.dart
@@ -14,78 +14,85 @@ class RadioConnectionScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final radioScanner = ref.watch(radioScannerProvider);
     final meshRadios = radioScanner.meshRadios;
-    final height = MediaQuery.of(context).size.height;
-    late final Widget body;
 
-    final connectedRadio = ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: 500),
-      child: const ConnectedRadio(),
-    );
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        late final Widget body;
 
-    final scanList = ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: 500),
-      child: Padding(
-        padding: const EdgeInsets.only(right: 20),
-        child: Scrollbar(
-          child: ListView.builder(
-            itemCount: meshRadios.length,
-            itemBuilder: (context, index) {
-              final radio = meshRadios[index];
-              return RadioChoiceTile(
-                radio: radio,
-              );
-            },
+        final connectedRadio = ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 500),
+          child: const ConnectedRadio(),
+        );
+
+        final scanList = ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 500),
+          child: Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: Scrollbar(
+              child: ListView.builder(
+                itemCount: meshRadios.length,
+                itemBuilder: (context, index) {
+                  final radio = meshRadios[index];
+                  return RadioChoiceTile(
+                    radio: radio,
+                  );
+                },
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        );
 
-    if (height < 500) {
-      body = Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [Flexible(child: connectedRadio), Flexible(child: scanList)],
-      );
-    } else {
-      body = SizedBox(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: Platform.isIOS
-              ? CrossAxisAlignment.center
-              : CrossAxisAlignment.start,
-          children: [connectedRadio, Expanded(child: scanList)],
-        ),
-      );
-    }
+        if (constraints.maxHeight < 500) {
+          body = Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(child: connectedRadio),
+              Flexible(child: scanList),
+            ],
+          );
+        } else {
+          body = SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: Platform.isIOS
+                  ? CrossAxisAlignment.center
+                  : CrossAxisAlignment.start,
+              children: [connectedRadio, Expanded(child: scanList)],
+            ),
+          );
+        }
 
-    return ScaffoldMessenger(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Radio Connection'),
-        ),
-        body: body,
-        floatingActionButton: CircleAvatar(
-          backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-          radius: 50,
-          child: radioScanner.scanning
-              ? const SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 3,
-                  ),
-                )
-              : IconButton(
-                  color: Theme.of(context).colorScheme.onPrimaryContainer,
-                  onPressed: () {
-                    ref.read(radioScannerProvider.notifier).scan();
-                  },
-                  icon: const Icon(
-                    Icons.search,
-                    size: 50,
-                  ),
-                ),
-        ),
-      ),
+        return ScaffoldMessenger(
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Radio Connection'),
+            ),
+            body: body,
+            floatingActionButton: CircleAvatar(
+              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+              radius: 50,
+              child: radioScanner.scanning
+                  ? const SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 3,
+                      ),
+                    )
+                  : IconButton(
+                      color: Theme.of(context).colorScheme.onPrimaryContainer,
+                      onPressed: () {
+                        ref.read(radioScannerProvider.notifier).scan();
+                      },
+                      icon: const Icon(
+                        Icons.search,
+                        size: 50,
+                      ),
+                    ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
