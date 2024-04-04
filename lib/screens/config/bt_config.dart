f228960e5eecd2a2388e2691490c991f2735365c
paulo <paulo@paulos-Mac-mini.local>
2024-04-04 19:52:21 +0800

    bt config

--- lib/screens/config/bt_config.dart
+++ lib/screens/config/bt_config.dart
@@ -20,12 +20,21 @@ class BtConfigScreen extends ConsumerStatefulWidget {
 
 class _BtConfigScreenState extends ConsumerState<BtConfigScreen> {
   late Config_BluetoothConfig _bluetoothConfig;
+  final _formKey = GlobalKey<FormState>();
+  final _pinController = TextEditingController();
 
   @override
   void initState() {
     _bluetoothConfig =
         ref.read(radioConfigServiceProvider).bluetoothConfig.deepCopy();
+    _pinController.text = _bluetoothConfig.fixedPin.toString();
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pinController.dispose();
+    super.dispose();
   }
 
   @override
@@ -41,74 +50,95 @@ class _BtConfigScreenState extends ConsumerState<BtConfigScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              SwitchListTile(
-                contentPadding: EdgeInsets.zero,
-                title: const Text('Enabled'),
-                value: true,
-                onChanged: (value) {},
-              ),
-              DropdownButtonFormField(
-                value: pairingModes[0],
-                decoration: const InputDecoration(label: Text('Pin type')),
-                items: [
-                  for (var i = 0; i < pairingModes.length; i++)
-                    DropdownMenuItem(
-                      value: pairingModes[i],
-                      child: Text(pairingModes[i].name),
-                    ),
-                ],
-                onChanged: (value) {},
-              ),
-              TextFormField(
-                textInputAction: TextInputAction.done,
-                onChanged: (_) {},
-                onSaved: (value) {},
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Input PIN';
-                  }
-                  if (value.length != 6) {
-                    return 'PIN must be six digits';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  floatingLabelBehavior: FloatingLabelBehavior.auto,
-                  alignLabelWithHint: true,
-                  labelText: 'PIN',
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SwitchListTile(
+                  contentPadding: EdgeInsets.zero,
+                  title: const Text('Enabled'),
+                  value: true,
+                  onChanged: (value) {
+                    setState(() {
+                      _bluetoothConfig.enabled = value;
+                    });
+                  },
                 ),
-              ),
-              const SizedBox(height: 16),
-              OutlinedButton.icon(
-                onPressed: radioConnectorState is Connected &&
-                        prevBluetoothConfig != _bluetoothConfig
-                    ? () async {
-                        // device will reset after setting lora config
-                        // so we do not have to update local config here.
-                        final confirmed = await showConfirmationDialog(
-                          context,
-                          'Save will reboot device. Continue?',
-                        );
-                        if (!confirmed) {
-                          return;
-                        }
-                        // TODO: add error handling
-                        await ref
-                            .read(radioConfigUploaderServiceProvider)
-                            .uploadBluetoothConfig(
-                              bluetoothConfig: _bluetoothConfig,
-                            );
-                        if (context.mounted) {
-                          context.go('/');
-                        }
+                DropdownButtonFormField(
+                  value: _bluetoothConfig.mode,
+                  decoration: const InputDecoration(label: Text('Pin type')),
+                  items: [
+                    for (var i = 0; i < pairingModes.length; i++)
+                      DropdownMenuItem(
+                        value: pairingModes[i],
+                        child: Text(pairingModes[i].name),
+                      ),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      _bluetoothConfig.mode = value!;
+                    });
+                  },
+                ),
+                if (_bluetoothConfig.mode ==
+                    Config_BluetoothConfig_PairingMode.FIXED_PIN)
+                  TextFormField(
+                    controller: _pinController,
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      setState(() {
+                        _bluetoothConfig.fixedPin = int.parse(value);
+                      });
+                    },
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return 'Input PIN';
                       }
-                    : null,
-                label: const Text('Save'),
-                icon: const Icon(Icons.power_settings_new_rounded),
-              ),
-            ],
+                      if (value.length != 6) {
+                        return 'PIN must be six digits';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      alignLabelWithHint: true,
+                      labelText: 'PIN',
+                    ),
+                  ),
+                const SizedBox(height: 16),
+                OutlinedButton.icon(
+                  onPressed: radioConnectorState is Connected &&
+                          prevBluetoothConfig != _bluetoothConfig
+                      ? () async {
+                          if (!_formKey.currentState!.validate()) {
+                            return;
+                          }
+                          // device will reset after setting lora config
+                          // so we do not have to update local config here.
+                          final confirmed = await showConfirmationDialog(
+                            context,
+                            'Save will reboot device. Continue?',
+                          );
+                          if (!confirmed) {
+                            return;
+                          }
+                          // TODO: add error handling
+                          await ref
+                              .read(radioConfigUploaderServiceProvider)
+                              .uploadBluetoothConfig(
+                                bluetoothConfig: _bluetoothConfig,
+                              );
+                          if (context.mounted) {
+                            context.go('/');
+                          }
+                        }
+                      : null,
+                  label: const Text('Save'),
+                  icon: const Icon(Icons.power_settings_new_rounded),
+                ),
+              ],
+            ),
           ),
         ),
       ),
